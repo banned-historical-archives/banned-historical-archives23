ba623b69636b4c364ca9622aed7150b5d8e59741
Your Name <you@example.com>
2024-09-17 12:42:30 +0800

    1

---
+++ a6b10242-e66a-4428-9c3c-99586b9a8eb2.ts
@@ -0,0 +1,81 @@
+export default {
+  "entity": {
+    "id": "a6b10242-e66a-4428-9c3c-99586b9a8eb2",
+    "name": "为印发反动大字报《关于社会主义的民主与法制》以供批判而写的按语",
+    "internal": false,
+    "type": "img",
+    "official": false,
+    "author": "中共广州市委批林批孔运动办公室印",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives23/main/a6b10242-e66a-4428-9c3c-99586b9a8eb2/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives23/main/a6b10242-e66a-4428-9c3c-99586b9a8eb2/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives23/main/a6b10242-e66a-4428-9c3c-99586b9a8eb2/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives23/main/a6b10242-e66a-4428-9c3c-99586b9a8eb2/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives23/main/a6b10242-e66a-4428-9c3c-99586b9a8eb2/5.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives23/main/a6b10242-e66a-4428-9c3c-99586b9a8eb2/6.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives23/main/a6b10242-e66a-4428-9c3c-99586b9a8eb2/7.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives23/main/a6b10242-e66a-4428-9c3c-99586b9a8eb2/8.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives23/main/a6b10242-e66a-4428-9c3c-99586b9a8eb2/9.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives23/main/a6b10242-e66a-4428-9c3c-99586b9a8eb2/10.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives23/main/a6b10242-e66a-4428-9c3c-99586b9a8eb2/11.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives23/main/a6b10242-e66a-4428-9c3c-99586b9a8eb2/12.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives23/main/a6b10242-e66a-4428-9c3c-99586b9a8eb2/13.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives23/main/a6b10242-e66a-4428-9c3c-99586b9a8eb2/14.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives23/main/a6b10242-e66a-4428-9c3c-99586b9a8eb2/15.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives23/main/a6b10242-e66a-4428-9c3c-99586b9a8eb2/16.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives23/main/a6b10242-e66a-4428-9c3c-99586b9a8eb2/17.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives23/main/a6b10242-e66a-4428-9c3c-99586b9a8eb2/18.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives23/main/a6b10242-e66a-4428-9c3c-99586b9a8eb2/19.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives23/main/a6b10242-e66a-4428-9c3c-99586b9a8eb2/20.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives23/main/a6b10242-e66a-4428-9c3c-99586b9a8eb2/21.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives23/main/a6b10242-e66a-4428-9c3c-99586b9a8eb2/22.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives23/main/a6b10242-e66a-4428-9c3c-99586b9a8eb2/23.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives23/main/a6b10242-e66a-4428-9c3c-99586b9a8eb2/24.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives23/main/a6b10242-e66a-4428-9c3c-99586b9a8eb2/25.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives23/main/a6b10242-e66a-4428-9c3c-99586b9a8eb2/26.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives23/main/a6b10242-e66a-4428-9c3c-99586b9a8eb2/27.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "为印发反动大字报《关于社会主义的民主与法制》以供批判而写的按语",
+        "authors": [
+          "中共广州市委批林批孔运动办公室印"
+        ],
+        "page_start": 2,
+        "page_end": 6,
+        "dates": [
+          {
+            "year": 1974,
+            "month": 12
+          }
+        ]
+      },
+      {
+        "title": "关于社会主义的民主与法制——献给毛主席和四届人大",
+        "authors": [
+          "李一哲"
+        ],
+        "page_start": 6,
+        "page_end": 27,
+        "dates": [
+          {
+            "year": 1974,
+            "month": 11
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "a6b10242-e66a-4428-9c3c-99586b9a8eb2",
+  "resource_type": "book",
+  "version": 2
+}
